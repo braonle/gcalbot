3e654c28e1c627b7694179ca7071b631c9f08fb8
Yaroslav Bondarenko <yaroslav.bondarenko@phystech.edu>
2022-07-12 19:39:06 +0300

    Initial project structure

---
+++ engine/global_params.py
@@ -0,0 +1,13 @@
+# Bot parameters
+TOKEN = <bot token>
+POLL_INTERVAL = 2.0
+
+# SQLite3 parameters
+DB_NAME = <SQLite DB path>
+
+# Webhook params
+PUBLIC_IP = <public IP>
+LISTEN_IP = <private IP>
+PORT = <TCP port>
+PRIVATE_KEY = <path to certificate private key>
+CERIFICATE = <path to certificate>
